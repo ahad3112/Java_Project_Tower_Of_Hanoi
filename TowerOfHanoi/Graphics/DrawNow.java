b38266713cb813f78435a87446975fcc363f461c
ahad3112 <ahad3112@yahoo.com>
2016-12-11 22:52:51 +0100

    Update DrawNow.java

--- TowerOfHanoi/Graphics/DrawNow.java
+++ TowerOfHanoi/Graphics/DrawNow.java
@@ -1,5 +1,10 @@
 package TowerOfHanoi.Graphics;
 
+/**
+ * Implementing the Tower of Hanoi classical algorithm
+ * @author Muhammed Ahad
+ * e-mail: ahad3112@yahoo.com
+ */
 
 import TowerOfHanoi.test.Plate.TowerPlate;
 import javax.swing.*;
